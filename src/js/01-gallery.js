695475d78e1505024572e372febfbc7ce3584758
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-14 21:49:10 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  boxGallery: document.querySelector(".gallery"),
+};
+
+const addGalleryItem = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+};
+refs.boxGallery.insertAdjacentHTML("beforeend", addGalleryItem());
+
+refs.boxGallery.addEventListener("click", onOpenImg);
+function onOpenImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImg = e.target.dataset.source;
+  // console.log(largeImg);
+
+  const instance = basicLightbox.create(`
+  <img src="${largeImg}">
+`);
+  instance.show();
+  window.addEventListener("keydown", onKey);
+  function onKey(e) {
+    if (e.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onKey);
+    }
+  }
+}
+
