aa5e57865aefecb1919d9d015c33acc34fa13e5a
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-15 13:10:20 +0300

    hw 8.1 +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,37 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
   boxGallery: document.querySelector(".gallery"),
 };
 
-const addGalleryItem = () => {
+function addGalleryItems() {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-        <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`;
+  <li>
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
     })
     .join("");
-};
-refs.boxGallery.insertAdjacentHTML("beforeend", addGalleryItem());
+}
 
-refs.boxGallery.addEventListener("click", onOpenImg);
-function onOpenImg(e) {
+refs.boxGallery.insertAdjacentHTML("beforeend", addGalleryItems());
+
+refs.boxGallery.addEventListener("click", onClickImg);
+function onClickImg(e) {
   e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return;
   }
-
-  const largeImg = e.target.dataset.source;
-  // console.log(largeImg);
-
-  const instance = basicLightbox.create(`
-  <img src="${largeImg}">
-`);
-  instance.show();
-  window.addEventListener("keydown", onKey);
-  function onKey(e) {
-    if (e.key === "Escape") {
-      instance.close();
-      window.removeEventListener("keydown", onKey);
-    }
-  }
 }
 
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
